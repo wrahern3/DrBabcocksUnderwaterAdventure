d3659cbafb8cc6b63abad3412aebe3bb8fc31f6d
babcockjamesd <babcock.james.d@gmail.com>
2015-03-30 16:45:40 -0500

    First commit

---
+++ New Unity Project/Assets/Myassests/FlickerLight.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class FlickerLight : MonoBehaviour {
+
+    private Light theLight;
+    float lightChange;
+    //private Light defaultLight;
+    
+    // Use this for initialization
+	void Start () {
+        theLight = this.GetComponent<Light>();
+        //defaultLight = this.GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        lightChange = Random.value;
+
+        if ((lightChange * 10) % 2 > 1)
+            theLight.intensity += lightChange;
+        else
+            theLight.intensity -= lightChange;
+	}
+}
---
+++ New Unity Project/Assets/SciFi_Door/Script/DoorOpenScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpenScript : MonoBehaviour {
+
+    Animation anim;
+    bool isOpen;
+    bool isNear;
+    
+    // Use this for initialization
+	void Start () {
+        var parent = transform.parent;
+        var parentDoor = parent.FindChild("door");
+        anim = parentDoor.GetComponent<Animation>();
+        isOpen = false;
+        isNear = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!anim.isPlaying)
+        {
+            if (isOpen && !isNear)
+            {
+                anim.Play("close");
+                isOpen = false;
+            }
+            else if (!isOpen && isNear)
+            {
+                anim.Play("open");
+                isOpen = true;
+            }
+        }
+	}
+
+    void OnTriggerEnter () 
+    {
+        isNear = true;
+    }
+
+    void OnTriggerExit()
+    {
+        isNear = false;
+    }
+}
